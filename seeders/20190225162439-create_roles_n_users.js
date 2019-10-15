3344395166aed22976314f68a9ae83934bf58327
Dr. Asis Hallab <a.hallab@fz-juelich.de>
2019-10-15 13:24:00 +0200

    First published version of BreedPatH-DB

--- seeders/20190225162439-create_roles_n_users.js
+++ seeders/20190225162439-create_roles_n_users.js
@@ -4,10 +4,11 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.query(
       'SELECT 1 FROM db_was_seeded LIMIT 0').then(function(x) {
-      console.log('Database was already seeded, no new records are inserted.')
-    }).catch(function(e) {
+      console.log(
+        'Database was already seeded, no new records are inserted.')
+    }).catch(async function(e) {
       // Database has not been seeded yet.
-      return queryInterface.bulkInsert('roles', [{
+      await queryInterface.bulkInsert('roles', [{
         name: 'admin',
         description: 'The administrator is allowed to create, read, update, and delete users and their roles.'
       }, {
@@ -16,28 +17,48 @@ module.exports = {
       }, {
         name: 'guest',
         description: 'A guest is allowed read access to all data models, excluding users and user-roles.'
-      }]).then(function(x) {
-        return queryInterface.bulkInsert('users', [{
-          email: 'a.hallab@fz-juelich.de',
-          password: 'admin'
-        }])
-      }).then(function(x) {
-        return queryInterface.sequelize.query(
-          'INSERT INTO role_to_users ("userId", "roleId") VALUES ' +
-          '( (SELECT (id) FROM users WHERE email = \'a.hallab@fz-juelich.de\'), ' +
-          '(SELECT (id) FROM roles WHERE name = \'admin\') ), ' +
-          '( (SELECT (id) FROM users WHERE email = \'a.hallab@fz-juelich.de\'), ' +
-          '(SELECT (id) FROM roles WHERE name = \'scientist\') ), ' +
-          '( (SELECT (id) FROM users WHERE email = \'a.hallab@fz-juelich.de\'), ' +
-          '(SELECT (id) FROM roles WHERE name = \'guest\') )'
-        )
-      }).then(function(x) {
-        return queryInterface.sequelize.query(
-          'CREATE TABLE db_was_seeded ( seeded INT )')
-      }).then(function(x) {
-        return queryInterface.sequelize.query(
+      }])
+      await queryInterface.bulkInsert('users',
+        [
+          {
+            email: "asis.hallab@gmail.com",
+            password: "admin"
+          },
+          {
+            email: "b.usadel@fz-juelich.de",
+            password: "Björn01!Usadel10"
+          },
+          {
+            email: "g.ponce@fz-juelich.de",
+            password: "Yaxal09!Ponce90"
+          },
+          {
+            email: "m.enders@npz-innovation.de",
+            password: "Matthias02!Enders20"
+          }
+        ]
+      )
+      let userIds = await queryInterface.sequelize.query(
+        'SELECT id FROM users')
+      let roleIds = await queryInterface.sequelize.query(
+        'SELECT id FROM roles')
+      await queryInterface.bulkInsert('role_to_users',
+        userIds[0].map((usr) => {
+          return roleIds[0].map((rl) => {
+            return {
+              "userId": usr.id,
+              "roleId": rl.id
+            }
+          })
+        }).reduce((acc, val) => {
+          return acc.concat(val)
+        })
+      )
+      await queryInterface.sequelize.query(
+        'CREATE TABLE db_was_seeded ( seeded INT )')
+      await queryInterface
+        .sequelize.query(
           'INSERT INTO db_was_seeded (seeded) VALUES (1)')
-      })
     })
   },
 

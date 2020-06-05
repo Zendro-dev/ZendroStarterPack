37417968a76e7a089af393e3211a7324b97d55b8
Thomas Voecking <thomas.voecking@auticon.de>
2020-06-05 12:40:17 +0200

    Rework the keyspace creation, set a container name for the cassandra server, set the heap size, set the cassandra server to restart always

--- initCassandra.sh
+++ initCassandra.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-cqlsh localhost -e "CREATE KEYSPACE IF NOT EXISTS default WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};"
+
+# Create default keyspace for single node cluster
+CQL="CREATE KEYSPACE IF NOT EXISTS sciencedb WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};"
+until echo $CQL | cqlsh; do
+  echo "cqlsh: Cassandra is unavailable - retry later"
+  sleep 2
+done &
+
+exec /docker-entrypoint.sh "$@"

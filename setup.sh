dabbfb2aa9ab4fee657dd7d5d720429f9ecf104d
cons <cons@pop-os.localdomain>
2020-07-02 14:59:32 +0200

    setup.sh use echo -e to correctly work with /bin/bash

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ RED='\033[0;31m'
 ## Clean directory checks
 #
 # Msg
-echo "\n${LGRAY}@@ Checking for clean directory...${NC}"
+echo -e "\n${LGRAY}@@ Checking for clean directory...${NC}"
 # Check if graphql-server or single-page-app directory or already exist. If yes abort execution
 # Check if graphql-server-latest-stable or single-page-app-latest-stable already exist (they should'nt). Delete them
 # . Delete latest-stable.tar.gz if it exists
@@ -35,7 +35,7 @@ fi
 if [ -d "graphql-server" ]
 then
     # Msg
-    echo "${LGRAY} Directory ./graphl-server already exists... ${RED}aborting${NC}"
+    echo -e "${LGRAY} Directory ./graphl-server already exists... ${RED}aborting${NC}"
     exit 1
 fi
 
@@ -47,7 +47,7 @@ fi
 if [ -d "single-page-app" ]
 then
     # Msg
-    echo "${LGRAY} Directory ./single-page-app already exists... ${RED}aborting${NC}"
+    echo -e "${LGRAY} Directory ./single-page-app already exists... ${RED}aborting${NC}"
     exit 1
 fi
 
@@ -56,12 +56,12 @@ then
     rm -r single-page-app-latest-stable
 fi
 # Msg
-echo "${LGRAY}@@ ...${LGREEN}done${NC}"
+echo -e "${LGRAY}@@ ...${LGREEN}done${NC}"
 
 ## Add graphql-server skeleton
 #
 # Msg
-echo "\n${LGRAY}@@ Adding graphql-server skeleton...${NC}"
+echo -e "\n${LGRAY}@@ Adding graphql-server skeleton...${NC}"
 # get the latest stable archive from github
 wget https://github.com/ScienceDb/graphql-server/archive/latest-stable.tar.gz
 # extract the folder from the archive
@@ -71,12 +71,12 @@ mv graphql-server-latest-stable graphql-server
 # remove the downloaded arvhice
 rm latest-stable.tar.gz
 # Msg
-echo "${LGRAY}@@ Adding graphql-server skeleton... ${LGREEN}done${NC}"
+echo -e "${LGRAY}@@ Adding graphql-server skeleton... ${LGREEN}done${NC}"
 
 ## Addsingle-page-app skeleton
 #
 # Msg
-echo "\n${LGRAY}@@ Adding single-page-app skeleton...${NC}"
+echo -e "\n${LGRAY}@@ Adding single-page-app skeleton...${NC}"
 # get the latest stable archive from github
 wget https://github.com/ScienceDb/single-page-app/archive/latest-stable.tar.gz
 # extract the folder from the archive
@@ -86,4 +86,4 @@ mv single-page-app-latest-stable single-page-app
 # remove the downloaded arvhice
 rm latest-stable.tar.gz
 # Msg
-echo "${LGRAY}@@ Adding single-page-app skeleton... ${LGREEN}done${NC}"
+echo -e "${LGRAY}@@ Adding single-page-app skeleton... ${LGREEN}done${NC}"

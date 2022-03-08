02845d7dcfd27070387634964f725fed3f140407
coeit <cbeiteneuer@gmail.com>
2022-03-08 16:51:12 +0100

    remove default postgres database

--- contexts/initUserDb.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER zendro WITH SUPERUSER PASSWORD 'zendro';
-CREATE DATABASE zendro_development OWNER zendro;
-CREATE DATABASE zendro_test OWNER zendro;
-CREATE DATABASE zendro_production OWNER zendro;

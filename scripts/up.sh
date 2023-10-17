0001985f327c86800369044abe6930eb824bbac5
107982913+qwerdenkerXD <107982913+qwerdenkerXD@users.noreply.github.com>
2023-10-17 10:53:47 +0200

    update: use docker compose plugin

--- scripts/up.sh
+++ scripts/up.sh
@@ -2,7 +2,7 @@
 
 # 1=dev|prod
 
-docker-compose -f docker-compose-$1.yml up -d \
+docker compose -f docker-compose-$1.yml up -d \
   --remove-orphans \
   --force-recreate \
   --renew-anon-volumes
